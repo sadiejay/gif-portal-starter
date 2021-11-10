a01f8e65ac030f4e72e3508dda19d6ef95a578f0
sadiejay <19538219+sadiejay@users.noreply.github.com>
2021-11-10 04:10:17 -0600

    Added input and button

--- src/App.js
+++ src/App.js
@@ -94,8 +94,17 @@ const [walletAddress, setWalletAddress] = useState(null);
 // Renders after wallet is connected
  const renderConnectedContainer = () => (
   <div className="connected-container">
+    {/* Go ahead and add this input and button to start */}
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
+      <input type="text" placeholder="Enter gif link" />
+      <button type="submit" className="cta-button submit-gif-button">Submit</button>
+    </form>
     <div className="gif-grid">
-      {TEST_GIFS.map(gif => (
+      {TEST_GIFS.map((gif) => (
         <div className="gif-item" key={gif}>
           <img src={gif} alt={gif} />
         </div>
